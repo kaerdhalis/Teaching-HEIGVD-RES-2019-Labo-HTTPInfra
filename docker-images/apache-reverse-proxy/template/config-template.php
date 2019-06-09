07e6b37f235118f95be70f1b819bbf5514622c22
kaerdhalis <benjamin.leguillou@heig-vd.ch>
2019-06-09 09:46:07 +0200

    add node balancing

--- docker-images/apache-reverse-proxy/template/config-template.php
+++ docker-images/apache-reverse-proxy/template/config-template.php
@@ -1,14 +1,26 @@
 <?php 
 $dynamic_app = getenv('DYNAMIC_APP');
 $static_app  = getenv('STATIC_APP');
+$dynamic_app2 = getenv('DYNAMIC_APP2');
+$static_app2  = getenv('STATIC_APP2');
 ?>
 
 <VirtualHost *:80>
 	ServerName demo.res.ch
 
-	ProxyPass '/api/cities/' 'http://<?php print $dynamic_app ?>/'
-	ProxyPassReverse '/api/cities/' 'http://<?php print $dynamic_app ?>/'
+	<Proxy "balancer://cities">
+    		BalancerMember "http://<?php print $dynamic_app ?>"
+    		BalancerMember "http://<?php print $dynamic_app2 ?>"
+	</Proxy>
 
-	ProxyPass '/' 'http://<?php print $static_app ?>/'
-	ProxyPassReverse '/' 'http://<?php print $static_app ?>/'
+	ProxyPass '/api/cities/' 'balancer://cities/'
+	ProxyPassReverse '/api/cities/' 'balancer://cities/'
+
+	<Proxy "balancer://static">
+    		BalancerMember "http://<?php print $static_app ?>"
+    		BalancerMember "http://<?php print $static_app2 ?>"
+	</Proxy>
+	
+	ProxyPass '/' 'balancer://static/'
+	ProxyPassReverse '/' 'balancer://static/'
 </VirtualHost>
